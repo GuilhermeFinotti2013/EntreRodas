04aaf062f994295c422abaa511d07acefbae94e3
GuilhermeFinotti2013 <guilhermelfinotti@gmail.com>
2019-09-20 17:38:12 -0300

    Criado CRUD de perfil de usuários

---
+++ Web/Web/Controllers/AspNetRolesController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AspNetRolesController : Controller
+    {
+        private entre_rodasEntities db = new entre_rodasEntities();
+
+        // GET: AspNetRoles
+        public ActionResult Index()
+        {
+            return View(db.AspNetRoles.ToList());
+        }
+
+        // GET: AspNetRoles/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AspNetRoles/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name")] AspNetRoles aspNetRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                AspNetRoles role = new AspNetRoles();
+                role.Id = Guid.NewGuid().ToString();
+                role.Name = aspNetRoles.Name.Trim();
+                db.AspNetRoles.Add(role);
+                db.SaveChanges();
+                
+                return RedirectToAction("Index");
+            }
+
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            AspNetRoles role = new AspNetRoles();
+            role.Id = aspNetRoles.Id;
+            role.Name = aspNetRoles.Name.Trim();
+            return View(role);
+        }
+
+        // POST: AspNetRoles/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] AspNetRoles aspNetRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                AspNetRoles role = new AspNetRoles();
+                role.Id = aspNetRoles.Id;
+                role.Name = aspNetRoles.Name.Trim();
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aspNetRoles);
+        }
+
+        // GET: AspNetRoles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            if (aspNetRoles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aspNetRoles);
+        }
+
+        // POST: AspNetRoles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            AspNetRoles aspNetRoles = db.AspNetRoles.Find(id);
+            db.AspNetRoles.Remove(aspNetRoles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
