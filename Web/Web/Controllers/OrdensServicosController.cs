def09d73c6411f379f704c74286f99ad24d7cc88
Guilherme Luis Finotti <guifinotti@hotmail.com>
2020-03-04 20:20:32 -0300

    .

--- Web/Web/Controllers/OrdensServicosController.cs
+++ Web/Web/Controllers/OrdensServicosController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -16,6 +18,10 @@ namespace Web.Controllers
     public class OrdensServicosController : Controller
     {
         private entre_rodasEntities db = new entre_rodasEntities();
+        private Font fonteSubtitulos = new Font(Font.FontFamily.TIMES_ROMAN, 13, Font.BOLD);
+        private Font fonteNegrito = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.BOLD);
+        private Font fonteNormal = new Font(Font.FontFamily.TIMES_ROMAN, 10);
+        private float larguraBordas = 1.0f;
 
         // GET: OrdensServicos
         public ActionResult Index(string Pesquisar = "")
@@ -484,6 +490,372 @@ namespace Web.Controllers
             }
             return valorTotal;
         }
+        #endregion
+
+        #region Gerar PDF
+        private OrdensServicos BuscarOrdem()
+        {
+            OrdensServicos ordem = new OrdensServicos();
+            ordem.CodigoOrdensServicos = "021-2020";
+            ordem.Veiculos = new Veiculos() { Modelo = "Kicks", Ano = 2019 };
+            ordem.Clientes = new Clientes() { Nome = "Guilherme Finotti" };
+            ordem.DataInicialPrevista = DateTime.Now;
+            ordem.ProblemaIdentificado = "O carro está derrabando nas lombas e com aquecimento.";
+            ordem.InformacoesAdicionais = "Informativo: Bomba de oleo R$256,50";
+            ordem.OrdensServicosServicos = new List<OrdensServicosServicos>();
+            ordem.OrdensServicosServicos.Add(new OrdensServicosServicos() { Descricao = "Mão de obra", Valor = 160.00f });
+            ordem.OrdensServicosServicos.Add(new OrdensServicosServicos() { Descricao = "Limpeza do cárter/t. Correia/filtro/arrefecimento", Valor = 230.00f });
+            ordem.OrdensServicosServicos.Add(new OrdensServicosServicos() { Descricao = "Troca de oleo", Valor = 100.00f });
+            ordem.OrdensServicosMateriais = new List<OrdensServicosMateriais>();
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Amortecedor dianteiro", Quantidade = 2, PrecoUnitario = 155.36f, PrecoTotal = 310.72f });
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Amortecedor traseiro", Quantidade = 2, PrecoUnitario = 166.45f, PrecoTotal = 332.90f });
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Kit correia dentada", Quantidade = 1, PrecoUnitario = 89.00f, PrecoTotal = 89.00f });
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Filtro combustível", Quantidade = 1, PrecoUnitario = 19.50f, PrecoTotal = 19.50f });
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Amortecedores tampa traseira", Quantidade = 2, PrecoUnitario = 49.00f, PrecoTotal = 98.00f });
+            ordem.OrdensServicosMateriais.Add(new OrdensServicosMateriais() { Descricao = "Aditivo concentrado para radiador", Quantidade = 2, PrecoUnitario = 25.00f, PrecoTotal = 50.00f });
+            return ordem;
+        }
+
+        public ActionResult GerarOrcamentoPdf()
+        {
+            OrdensServicos ordem = this.BuscarOrdem();
+
+            Document pdfDoc = new Document(PageSize.A4, 10, 10, 80, 80);
+            PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+            Image imagemLogo = Image.GetInstance(Request.MapPath("~/Content/Imagens/logo.png"));
+            Image imagemGmail = Image.GetInstance(Request.MapPath("~/Content/Imagens/gmail.png"));
+            Image imagemWhats = Image.GetInstance(Request.MapPath("~/Content/Imagens/whats.png"));
+            Image imagemMaps = Image.GetInstance(Request.MapPath("~/Content/Imagens/maps.png"));
+            pdfWriter.PageEvent = new ITextEvents() { ImagemLogo = imagemLogo, ImagemEmail = imagemGmail, ImagemWhats = imagemWhats, ImagemMaps = imagemMaps };
+            pdfDoc.Open();
+
+
+
+
+            PdfPCell celunaVazia;
+            PdfPCell celunaConteudo;
+
+            #region Cabeçalho
+            PdfPTable tabela;
+            PdfPCell celula;
+            /* = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            tabela.HorizontalAlignment = 0;
+            tabela.SpacingAfter = 7f;
+            PdfPCell celula = new PdfPCell(new Phrase("Entre Rodas - Assistência técnica automotiva", new Font(Font.FontFamily.TIMES_ROMAN, 8, Font.BOLD)));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);*/
+            //Table
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            tabela.SpacingBefore = 5f;
+            //Cell
+            celula = new PdfPCell(new Phrase(String.Format("ORÇAMENTO {0}", ordem.CodigoOrdensServicos), new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.BOLD)));
+            celula.FixedHeight = 30.0f;
+            celula.Border = 0;
+            celula.BackgroundColor = new BaseColor(221, 221, 221);
+            celula.VerticalAlignment = Element.ALIGN_TOP;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+
+            #endregion
+            #region Informações da ordem de serviço
+            pdfDoc.Add(this.MontarLinhaSubtitulo("INFORMAÇÕES DA ORDEM DE SERVIÇOS", 0, true));
+            tabela = new PdfPTable(4);
+            tabela.WidthPercentage = 100;
+            float[] larguras = new float[] { 6f, 44f, 10f, 20f };
+            tabela.SetWidths(larguras);
+            celula = new PdfPCell(new Phrase("Cliente:", fonteNegrito));
+            celula.Border = 0;
+            celula.BorderWidthLeft = larguraBordas;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase(ordem.Clientes.Nome, fonteNormal));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            celula = new PdfPCell(new Phrase("Modelo/Ano:", fonteNegrito));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase(String.Format("{0} {1}", ordem.Veiculos.Modelo, ordem.Veiculos.Ano), fonteNormal));
+            celula.Border = 0;
+            celula.BorderWidthRight = larguraBordas;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+
+            tabela = new PdfPTable(4);
+            tabela.WidthPercentage = 100;
+            larguras = new float[] { 16f, 30f, 14f, 20 };
+            tabela.SetWidths(larguras);
+            celula = new PdfPCell(new Phrase("Funcionário responsável:", fonteNegrito));
+            celula.Border = 0;
+            celula.BorderWidthLeft = larguraBordas;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase("Alan", fonteNormal));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            celula = new PdfPCell(new Phrase("Data do orçamento:", fonteNegrito));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase(ordem.DataInicialPrevista.Value.ToString("dd/MM/yyyy"), fonteNormal));
+            celula.Border = 0;
+            celula.BorderWidthRight = 1.0f;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            celula = new PdfPCell(new Phrase("Problema identificado junto ao cliente:", fonteNegrito));
+            celula.Border = 0;
+            celula.BorderWidthLeft = larguraBordas;
+            celula.BorderWidthRight = larguraBordas;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            celula = new PdfPCell(new Phrase(ordem.ProblemaIdentificado, new Font(Font.FontFamily.TIMES_ROMAN, 12)));
+            celula.Border = 0;
+            celula.BorderWidthBottom = larguraBordas;
+            celula.BorderWidthLeft = larguraBordas;
+            celula.BorderWidthRight = larguraBordas;
+            celula.PaddingBottom = 8f;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            #endregion
+            #region Serviços            
+            pdfDoc.Add(this.MontarLinhaSubtitulo("SERVIÇOS", 390.00, true));
+            pdfDoc.Add(this.MontarCabecalhoTabelaDeItens(TipoItens.Servicos));
+            tabela = new PdfPTable(4);
+            tabela.WidthPercentage = 100;
+            larguras = new float[] { 1f, 75f, 10f, 1f };
+            tabela.SetWidths(larguras);
+            //Percorre a lista de serviços
+            for (int i = 0; i < ordem.OrdensServicosServicos.Count; i++)
+            {
+                celunaVazia = new PdfPCell();
+                celunaVazia.FixedHeight = 15.0f;
+                celunaVazia.Border = 0;
+                celunaVazia.BorderWidthLeft = larguraBordas;
+                if (i + 1 == ordem.OrdensServicosServicos.Count)
+                {
+                    celunaVazia.BorderWidthBottom = larguraBordas;
+                }
+                tabela.AddCell(celunaVazia);
+                celunaConteudo = new PdfPCell(new Phrase(ordem.OrdensServicosServicos.ToList()[i].Descricao, fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaConteudo = new PdfPCell(new Phrase(String.Format("R${0}", ordem.OrdensServicosServicos.ToList()[i].Valor), fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+
+                celunaConteudo.HorizontalAlignment = Element.ALIGN_RIGHT;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaVazia = new PdfPCell();
+                celunaVazia.FixedHeight = 15.0f;
+                celunaVazia.Border = 0;
+                celunaVazia.BorderWidthRight = larguraBordas;
+                if (i + 1 == ordem.OrdensServicosServicos.Count)
+                {
+                    celunaVazia.BorderWidthBottom = larguraBordas;
+                }
+                tabela.AddCell(celunaVazia);
+            }
+            pdfDoc.Add(tabela);
+            #endregion
+            #region Materiais
+            pdfDoc.Add(this.MontarLinhaSubtitulo("MATERIAIS", 900.12, true));
+            pdfDoc.Add(this.MontarCabecalhoTabelaDeItens(TipoItens.Materiais));
+            tabela = new PdfPTable(6);
+            tabela.WidthPercentage = 100;
+            larguras = new float[] { 1f, 45f, 15f, 15f, 10f, 1f };
+            tabela.SetWidths(larguras);
+            //Percorre a lista de serviços
+            for (int i = 0; i < ordem.OrdensServicosMateriais.Count; i++)
+            {
+                celunaVazia = new PdfPCell();
+                celunaVazia.FixedHeight = 15.0f;
+                celunaVazia.Border = 0;
+                celunaVazia.BorderWidthLeft = larguraBordas;
+                if (i + 1 == ordem.OrdensServicosMateriais.Count)
+                {
+                    celunaVazia.BorderWidthBottom = larguraBordas;
+                }
+                tabela.AddCell(celunaVazia);
+                celunaConteudo = new PdfPCell(new Phrase(ordem.OrdensServicosMateriais.ToList()[i].Descricao, fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaConteudo = new PdfPCell(new Phrase(ordem.OrdensServicosMateriais.ToList()[i].Quantidade.ToString(), fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaConteudo = new PdfPCell(new Phrase(String.Format("R${0}", ordem.OrdensServicosMateriais.ToList()[i].PrecoUnitario), fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaConteudo = new PdfPCell(new Phrase(String.Format("R${0}", ordem.OrdensServicosMateriais.ToList()[i].PrecoTotal), fonteNormal));
+                celunaConteudo.FixedHeight = 15.0f;
+                celunaConteudo.Border = 0;
+                celunaConteudo.HorizontalAlignment = Element.ALIGN_RIGHT;
+                celunaConteudo.BorderWidthBottom = larguraBordas;
+                tabela.AddCell(celunaConteudo);
+                celunaVazia = new PdfPCell();
+                celunaVazia.FixedHeight = 15.0f;
+                celunaVazia.Border = 0;
+                celunaVazia.BorderWidthRight = larguraBordas;
+                if (i + 1 == ordem.OrdensServicosMateriais.Count)
+                {
+                    celunaVazia.BorderWidthBottom = larguraBordas;
+                }
+                tabela.AddCell(celunaVazia);
+            }
+            pdfDoc.Add(tabela);
+            #endregion
+            pdfDoc.Add(this.MontarLinhaSubtitulo("Total", 1290.12, false));
+            #region Informações adicionais
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            celula = new PdfPCell(new Phrase("Informações adicionais", fonteNegrito));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            celula = new PdfPCell(new Phrase(ordem.InformacoesAdicionais, new Font(Font.FontFamily.TIMES_ROMAN, 12)));
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            #endregion
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            tabela.SpacingBefore = 10f;
+            celula = new PdfPCell(new Phrase(String.Format("São Leopoldo, {0}", DateTime.Now.ToString("dd/MM/yyyy"), fonteNormal)));
+            celula.Border = 0;
+            celula.HorizontalAlignment = Element.ALIGN_CENTER;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            tabela = new PdfPTable(1);
+            tabela.WidthPercentage = 100;
+            tabela.SpacingBefore = 10f;
+            celula = new PdfPCell(new Phrase("Entre Rodas Assistência Técnica Automotiva", fonteNegrito));
+            celula.Border = 0;
+            celula.HorizontalAlignment = Element.ALIGN_CENTER;
+            tabela.AddCell(celula);
+            pdfDoc.Add(tabela);
+            pdfWriter.CloseStream = false;
+            pdfDoc.Close();
+            Response.Buffer = true;
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", "attachment;filename=Credit-Card-Report.pdf");
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Write(pdfDoc);
+            Response.End();
+
+            return View();
+        }
+
+        private PdfPTable MontarLinhaSubtitulo(String titulo, Double valorTotal, bool ehTabela)
+        {
+            PdfPTable tabela = new PdfPTable(2);
+            tabela.WidthPercentage = 100;
+            float[] larguras = new float[] { 65f, 20f };
+            tabela.SetWidths(larguras);
+            tabela.SpacingBefore = 5f;
+            PdfPCell celula = new PdfPCell(new Phrase(titulo, fonteSubtitulos));
+            celula.FixedHeight = 20.0f;
+            celula.Border = 0;
+            if (ehTabela)
+            {
+                celula.BorderWidthTop = larguraBordas;
+                celula.BorderWidthLeft = larguraBordas;
+            }
+            celula.BackgroundColor = new BaseColor(242, 242, 242);
+            celula.VerticalAlignment = Element.ALIGN_TOP;
+            tabela.AddCell(celula);
+            if (valorTotal == 0)
+            {
+                celula = new PdfPCell();
+            }
+            else
+            {
+                if (ehTabela)
+                {
+                    celula = new PdfPCell(new Phrase(String.Format("Subtotal: R${0}", valorTotal), fonteNegrito));
+                }
+                else
+                {
+                    celula = new PdfPCell(new Phrase(String.Format("R${0}", valorTotal), fonteNegrito));
+                }
+            }
+            celula.FixedHeight = 20.0f;
+            celula.Border = 0;
+            if (ehTabela)
+            {
+                celula.BorderWidthTop = larguraBordas;
+                celula.BorderWidthRight = larguraBordas;
+            }
+            celula.BackgroundColor = new BaseColor(242, 242, 242);
+            tabela.AddCell(celula);
+            return tabela;
+        }
+
+        private PdfPTable MontarCabecalhoTabelaDeItens(TipoItens tipo)
+        {
+            PdfPTable tabela = new PdfPTable(6);
+            tabela.WidthPercentage = 100;
+            float[] larguras = new float[] { 1f, 45f, 15f, 15f, 10f, 1f };
+            tabela.SetWidths(larguras);
+            PdfPCell celula = new PdfPCell();
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            celula.BorderWidthLeft = larguraBordas;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase("Descrição", fonteNegrito));
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            if (tipo == TipoItens.Materiais)
+            {
+                celula = new PdfPCell(new Phrase("Preço unitário", fonteNegrito));
+            }
+            else
+            {
+                celula = new PdfPCell();
+            }
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            if (tipo == TipoItens.Materiais)
+            {
+                celula = new PdfPCell(new Phrase("Quantidade", fonteNegrito));
+            }
+            else
+            {
+                celula = new PdfPCell();
+            }
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            tabela.AddCell(celula);
+            celula = new PdfPCell(new Phrase("Valor", fonteNegrito));
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            celula.HorizontalAlignment = Element.ALIGN_RIGHT;
+            tabela.AddCell(celula);
+            celula = new PdfPCell();
+            celula.FixedHeight = 15.0f;
+            celula.Border = 0;
+            celula.BorderWidthRight = larguraBordas;
+            tabela.AddCell(celula);
+
+            return tabela;
+        }
+
         #endregion
     }
 }
---
+++ Web/Web/Util/iTextEvents.cs
@@ -0,0 +1,130 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Util
+{
+    public class ITextEvents : PdfPageEventHelper
+    {
+        // This is the contentbyte object of the writer
+        PdfContentByte cb;
+
+        // we will put the final number of pages in a template
+        PdfTemplate headerTemplate, footerTemplate;
+
+        // this is the BaseFont we are going to use for the header / footer
+        BaseFont bf = null;
+
+        // This keeps track of the creation time
+        DateTime PrintTime = DateTime.Now;
+
+        public Image ImagemLogo { get; set; }
+        public Image ImagemWhats { get; set; }
+        public Image ImagemEmail { get; set; }
+        public Image ImagemMaps { get; set; }
+
+        #region Fields
+        private string _header;
+        #endregion
+
+        #region Properties
+        public string Header
+        {
+            get { return _header; }
+            set { _header = value; }
+        }
+        #endregion
+
+        public override void OnOpenDocument(PdfWriter writer, Document document)
+        {
+            try
+            {
+                PrintTime = DateTime.Now;
+                bf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+                cb = writer.DirectContent;
+                headerTemplate = cb.CreateTemplate(100, 100);
+                footerTemplate = cb.CreateTemplate(50, 50);
+            }
+            catch (DocumentException de)
+            {
+            }
+            catch (System.IO.IOException ioe)
+            {
+            }
+        }
+
+        public override void OnEndPage(iTextSharp.text.pdf.PdfWriter writer, iTextSharp.text.Document document)
+        {
+            base.OnEndPage(writer, document);
+            iTextSharp.text.Font baseFontNormal = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12f, iTextSharp.text.Font.NORMAL, iTextSharp.text.BaseColor.BLACK);
+            iTextSharp.text.Font baseFontBig = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK);
+            Font fonteNormal = new Font(Font.FontFamily.TIMES_ROMAN, 7, Font.BOLD);
+
+            //Create PdfTable object
+            PdfPTable pdfTab = new PdfPTable(3);
+            pdfTab.TotalWidth = document.PageSize.Width - 80f;
+            pdfTab.WidthPercentage = 100;
+            float[] larguras = new float[] { 2f, 0.5f, 7f };
+            pdfTab.SetWidths(larguras);
+            PdfPCell celulaIcone;
+            PdfPCell celulaTexto;
+            PdfPCell celulaLogo;
+
+            celulaLogo = new PdfPCell();
+            ImagemLogo.ScaleAbsolute(100, 50);
+            celulaLogo.AddElement(ImagemLogo);
+            celulaLogo.Border = 0;
+            celulaLogo.Rowspan = 3;
+            pdfTab.AddCell(celulaLogo);
+            celulaIcone = new PdfPCell();
+            celulaIcone.Border = 0;
+            ImagemEmail.ScaleAbsolute(12, 12);
+            celulaIcone.AddElement(ImagemEmail);
+            pdfTab.AddCell(celulaIcone);
+            celulaTexto = new PdfPCell(new Phrase("entrerodasautomotiva@gmail.com", fonteNormal));
+            celulaTexto.Border = 0;
+            pdfTab.AddCell(celulaTexto);
+
+            celulaIcone = new PdfPCell();
+            celulaIcone.Border = 0;
+            ImagemWhats.ScaleAbsolute(12, 12);
+            celulaIcone.AddElement(ImagemWhats);
+            pdfTab.AddCell(celulaIcone);
+            celulaTexto = new PdfPCell(new Phrase("(51) 00035-3739", fonteNormal));
+            celulaTexto.Border = 0;
+            pdfTab.AddCell(celulaTexto);
+
+            celulaIcone = new PdfPCell();
+            celulaIcone.Border = 0;
+            ImagemMaps.ScaleAbsolute(12, 12);
+            celulaIcone.AddElement(ImagemMaps);
+            pdfTab.AddCell(celulaIcone);
+            celulaTexto = new PdfPCell(new Phrase("Rua Hoefel Sander, 165, Fazenda São Borja - São Leopoldo - RS | CEP 93044-830", fonteNormal));
+            celulaTexto.Border = 0;
+            pdfTab.AddCell(celulaTexto);
+
+            //Add paging to footer
+            {
+                cb.BeginText();
+                cb.SetFontAndSize(bf, 10);
+                cb.SetTextMatrix(document.PageSize.GetLeft(190), document.PageSize.GetBottom(30));
+                cb.ShowText("Entre Rodas Assistência Técnica Automotiva");
+                cb.EndText();
+                float len = bf.GetWidthPoint("text", 12);
+                cb.AddTemplate(footerTemplate, document.PageSize.GetRight(80) + len, document.PageSize.GetBottom(30));
+            }
+            pdfTab.WriteSelectedRows(0, -1, 110, document.PageSize.Height - 10, writer.DirectContent);
+            //Move the pointer and draw line to separate header section from rest of page
+            cb.MoveTo(40, document.PageSize.Height - 70);
+            cb.LineTo(document.PageSize.Width - 40, document.PageSize.Height - 70);
+            cb.Stroke();
+            //Move the pointer and draw line to separate footer section from rest of page
+            cb.MoveTo(40, document.PageSize.GetBottom(50));
+            cb.LineTo(document.PageSize.Width - 40, document.PageSize.GetBottom(50));
+            cb.Stroke();
+        }
+    }
+}
